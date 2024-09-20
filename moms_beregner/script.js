47a7550c1a11be46b0dfaa8c34ce05eeb620d40c
Ronja <roba0001@stud.kea.dk>
2024-09-20 08:46:11 +0200

    done

--- moms_beregner/script.js
+++ moms_beregner/script.js
@@ -1,2 +1,9 @@
-let beloeb;
-let moms = 25;
+"use strict";
+
+beregnMoms(100, 25);
+
+function beregnMoms(beloeb, moms) {
+  console.log(
+    `momsen ligger på ${moms}%, derfor er beløbet med moms: ${beloeb * (1 + moms / 100)}kr`
+  );
+}
