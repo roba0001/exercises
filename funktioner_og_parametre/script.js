7c1b1c3a523c85bdee8ce2245c5a5ed8b792b7b7
Ronja <roba0001@stud.kea.dk>
2024-09-20 08:29:52 +0200

    x

---
+++ funktioner_og_parametre/script.js
@@ -0,0 +1,18 @@
+// const myName = "Ronja";
+
+// const myPet = "dog";
+// const myPetsName = "Otto";
+
+// presentPet(myPet, myPetsName, myName);
+// // presentPet("Ronja", "dog", "Otto");
+// function presentPet(firstName, animalType, animalName) {
+//   //   document.querySelector("#input").textContent = `Hello ${firstName}`;
+//   console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}`);
+// }
+
+sayHello(firstName);
+
+function sayHello(firstName) {
+  let firstName = "din far";
+  console.log(`Hello ${firstName}`);
+}
