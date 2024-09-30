4a1574e6a6a6c71acf688ca1dce9694ddd223c5a
Ronja <roba0001@stud.kea.dk>
2024-09-30 15:09:57 +0200

    bar chart

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,11 @@
 const list = document.querySelector("ul");
-
-const li = document.createElement("li");
-li.style.setProperty("--height", "30");
-list.appendChild(li);
+const valueArr = [];
+setInterval(generateBars, 1000);
+function generateBars() {
+  const ranNum = Math.floor(Math.random() * 100);
+  valueArr.push(ranNum);
+  console.log(valueArr);
+  //   const li = document.createElement("li");
+  //   li.style.setProperty("--height", "30");
+  //   list.appendChild(li);
+}
