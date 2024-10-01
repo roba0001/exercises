b0bdcb26a467afb3c8e1c9eebc774f4bb0b4a7e1
Ronja <roba0001@stud.kea.dk>
2024-10-01 12:29:49 +0200

    barchart done

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,11 +1,17 @@
 const list = document.querySelector("ul");
 const valueArr = [];
-setInterval(generateBars, 1000);
+setInterval(generateBars, 1000); // start setinterval der kører funktionen generateBars hvert 1000ms
+
 function generateBars() {
-  const ranNum = Math.floor(Math.random() * 100);
-  valueArr.push(ranNum);
-  console.log(valueArr);
-  //   const li = document.createElement("li");
-  //   li.style.setProperty("--height", "30");
-  //   list.appendChild(li);
+  const ranNum = Math.floor(Math.random() * 100); //Generer tilfældigt tal 0-100
+
+  if (valueArr.length >= 20) {
+    valueArr.shift(); //hvis arrayets længde er større end eller lig med 20 fjernes det første element i arrayet
+    list.removeChild(list.firstElementChild); //vi fjerner også det første barn i list/ul
+  }
+  valueArr.push(ranNum); //tilføj ranNum til slutningen af arrayet
+
+  const li = document.createElement("li");
+  li.style.setProperty("--height", ranNum); //gør søjlens højde til ranNum
+  list.appendChild(li); //tilføj li børn til listen (ul)
 }
