fff98f140e40e43ac324ef965a42a26e91ba0c62
Ronja <roba0001@stud.kea.dk>
2024-10-01 15:08:32 +0200

    filter tabel

--- filter_oevelse_starter_files_table/script.js
+++ filter_oevelse_starter_files_table/script.js
@@ -10,6 +10,9 @@ const vehicles = [
   { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Jonas" },
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
+
+const tbodyPointer = document.querySelector("tbody");
+console.log("tbodyPointer", tbodyPointer);
 // FILTRERING
 
 // all electric vehicles
@@ -47,22 +50,69 @@ const bigRugbrodVehicle = vehicles.filter(isVehicleBigAndRugbrod);
 // ----------------------------------------------
 
 //specificerer hvor i dommen det skal
-const tbodyPointer = document.querySelector("tbody");
 
-// kører funktionen vis disse vehicles med parameter for hvilke vehicles
-showTheseVehicles(bigRugbrodVehicle);
+// kører funktionen vis disse vehicles med parameter (CONST) for hvilke vehicles
+showTheseVehicles(vehicles);
 
 // putter det ind i dommen
 function showTheseVehicles(arr) {
+  tbodyPointer.innerHTML = "";
+
   arr.forEach((each) => {
     tbodyPointer.innerHTML += `<tr>
   <td>${each.type}</td>
   <td>${each.fuel}</td>
   <td>${each.passengers}</td> 
-  <td>${each.stops}</td>
-  <td>${each.ownedBy}</td>
-  <td>${each.isElectric}</td>
-  <td>${each.isTandem}</td>
+  <td>${areYouChecked(each.stops)}</td>
+  <td>${areYouChecked(each.ownedBy)}</td>
+  <td>${areYouChecked(each.isElectric)}</td>
+  <td>${areYouChecked(each.isTandem)}</td>
 </tr>`;
   });
+}
+
+// funktion der checker om data er defined eller undefined
+function areYouChecked(data) {
+  // hvis data er true, sæt et x
+  if (data === true) {
+    return "X";
+    // hvis data er undefined, gør den tom
+  } else if (data === undefined) {
+    return " ";
+  } else {
+    // ellers sæt data ind
+    return data;
+  }
+}
+// KNAPPER
+// const button = document.querySelector("button");
+// function checkButtonClick() {
+//   const button = document.querySelector("button");
+
+//   if (button.classlist === ".unfiltered") {
+//     console.log("show all");
+//   } else if (button.classlist === ".elVehicle") {
+//     console.log("show all electric");
+//   }
+// }
+// document.querySelector(".unfiltered").addEventListener("click", showTheseVehicles);
+// document.querySelector(".elVehicle").addEventListener("click", showTheseVehicles);
+document.querySelectorAll("button").forEach((elm) => {
+  console.log("elm", elm);
+  elm.addEventListener("click", buttonClicks);
+});
+function buttonClicks(evt) {
+  const whatFilter = evt.currentTarget.dataset.filter;
+  console.log("whatFilter", whatFilter);
+  if (whatFilter === "all") {
+    showTheseVehicles(vehicles);
+  } else if (whatFilter === "elVehicle") {
+    showTheseVehicles(allElectricArr);
+  } else if (whatFilter === "moreThanTwoSeats") {
+    showTheseVehicles(moreThanTwoArr);
+  } else if (whatFilter === "elVehicleJonas") {
+    showTheseVehicles(electricVehicleAndOwnedByJonas);
+  } else if (whatFilter === "rugbrodVehicle") {
+    showTheseVehicles(bigRugbrodVehicle);
+  }
 }
