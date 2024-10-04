3a3156446e7bcfbc4ee5013a100e173d17c5c76b
Ronja <roba0001@stud.kea.dk>
2024-10-04 10:53:52 +0200

    3cap done

---
+++ 3capitalization/capitalization3.js
@@ -0,0 +1,8 @@
+const myName = "PEtER";
+const thirdLetter = myName[2];
+const myNameCap =
+  myName.substring(0, 2).toLowerCase() +
+  myName[2].toUpperCase() +
+  myName.substring(3).toLowerCase();
+
+console.log(myNameCap);
