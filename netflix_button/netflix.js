592915d2a98d7571d715c71b44bc6d92326876ac
Ronja <roba0001@stud.kea.dk>
2024-10-10 11:16:55 +0200

    kommenteret

--- netflix_button/netflix.js
+++ netflix_button/netflix.js
@@ -1,30 +1,30 @@
 startAnimation();
 
-document.querySelector("button").addEventListener("animationend", eventHandler);
-
 function startAnimation() {
   document.querySelector("html").addEventListener("mousemove", eventHandler);
   document.querySelector("button").addEventListener("animationend", eventHandler);
   document.querySelector("button").addEventListener("click", eventHandler);
+  // mus bevæges over html, animation slutter på button og button klikkes --> kør eventHandler
 }
 
 function eventHandler(evt) {
-  let event = evt.type;
+  let event = evt.type; //which event set off the function
   console.log(event);
   if (event === "click" || event === "animationend") {
-    nextScreen();
+    //hvis event er klik eller animationend,
+    nextScreen(); // så kør funktionen nextScreen
   } else if (event === "mousemove") {
-    notNextScreen();
+    //ellers hvis event er mousemove,
+    notNextScreen(); //så kør funktionen notNextScreen
   }
 }
 
 function nextScreen() {
-  document.querySelector("button").classList.add("hide");
-  document.querySelector("html").style.setProperty("background-color", "red");
-  document.querySelector("h1").textContent = "NEXT EPISODE";
+  document.querySelector("button").classList.add("hide"); //skjul button
+  document.querySelector("html").style.setProperty("background-color", "red"); //gør baggrund rød
+  document.querySelector("h1").textContent = "NEXT EPISODE"; // gør textContent for h1 til "NEXT EPISODE"
 }
 
 function notNextScreen() {
-  console.log("notNextScreen");
-  document.querySelector("button").removeEventListener("animationend", eventHandler);
+  document.querySelector("button").removeEventListener("animationend", eventHandler); //fjern eventlistener for animationend
 }
